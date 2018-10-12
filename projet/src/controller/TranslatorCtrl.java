6ba6fe7f35fd335ab501044373625830c1e48fe1
Mathieu Ringenbach <math.ringenbach@gmail.com>
2018-10-12 15:45:02 +0200

    Bip et arbre

--- projet/src/controller/TranslatorCtrl.java
+++ projet/src/controller/TranslatorCtrl.java
@@ -1,16 +1,27 @@
 package controller;
 
+import javax.sound.sampled.*;
+
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 
+import model.Arbre;
+
+/**
+ * classe Translator Controller
+ * @author Mathieu
+ * 
+ */
 public class TranslatorCtrl implements EventHandler<ActionEvent> {
 
 	@FXML
 	TextArea txtLetters;
 	@FXML
 	TextArea txtMorse;
+	
+	public static float SAMPLE_RATE = 8000f; 
 	
 	@Override
 	public void handle(ActionEvent arg0) {
@@ -21,6 +32,8 @@ public class TranslatorCtrl implements EventHandler<ActionEvent> {
 	@FXML
 	public void OnMorseClick() {
 		System.out.println(txtMorse.getText());
+		//Arbre nd = new Arbre();
+		//nd.getLetter(txtMorse.getText());
 	}
 	
 	@FXML
@@ -29,8 +42,21 @@ public class TranslatorCtrl implements EventHandler<ActionEvent> {
 	}
 
 	@FXML
-	public void OnPlayClick() {
+	public void OnPlayClick() throws InterruptedException, LineUnavailableException {
 		//jouer le son
+		String morse = txtMorse.getText();
+		for (int i = 0; i < morse.length(); i++) {
+			String c = morse.substring(i,  i+1);
+			if (c.equals(".")) {
+				tone(2500, 100, 1.0);
+			}
+			else if (c.equals("-")) {
+				tone(2500, 200, 1.0);
+			}
+			else if (c.equals(" ")) {
+				Thread.sleep(100);
+			}
+		}
 	}
 	
 	@FXML
@@ -45,4 +71,27 @@ public class TranslatorCtrl implements EventHandler<ActionEvent> {
 		txtMorse.setText(txt);
 		txtMorse.positionCaret(txtMorse.getText().length()+1);
 	}
+	
+	public static void tone(int hz, int msecs, double vol) throws LineUnavailableException 
+	{ 
+		byte[] buf = new byte[1]; 
+		AudioFormat af = 
+		new AudioFormat(
+			SAMPLE_RATE, // sampleRate 
+			8,   // sampleSizeInBits 
+			1,   // channels 
+			true,  // signed 
+			false);  // bigEndian 
+		SourceDataLine sdl = AudioSystem.getSourceDataLine(af); 
+		sdl.open(af); 
+		sdl.start(); 
+		for (int i=0; i < msecs*8; i++) { 
+			double angle = i/(SAMPLE_RATE/hz) * 2.0 * Math.PI; 
+			buf[0] = (byte)(Math.sin(angle) * 127.0 * vol); 
+			sdl.write(buf,0,1); 
+		} 
+		sdl.drain(); 
+		sdl.stop(); 
+		sdl.close(); 
+	} 
 }
--- projet/src/model/Arbre.java
+++ projet/src/model/Arbre.java
@@ -21,12 +21,23 @@ public class Arbre{
 	
 	/**
 	 * Constructeur de la classe Arbre
-	 * @param elem : valeur contenue dans le Noeud
-	 * @param gauche : Noeud de gauche (correspond à un ".")
-	 * @param droite : Noeud de droite (correspond à un "-")
+	 * @param elem valeur contenue dans le Noeud
+	 * @param gauche Noeud de gauche (correspond à un ".")
+	 * @param droite Noeud de droite (correspond à un "-")
 	 */
 	public Arbre(Object elem, Arbre gauche, Arbre droite) {
 		this.elem = elem;
+		this.gauche = gauche;
+		this.droite = droite;
+	}
+	
+	/**
+	 * Constructeur du noeud racine
+	 * @param gauche Noeud de gauche (correspond à un ".")
+	 * @param droite Noeud de droite (correspond à un "-")
+	 */
+	public Arbre(Arbre gauche, Arbre droite) {
+		this.elem = null;
 		this.gauche = gauche;
 		this.droite = droite;
 	}
@@ -45,6 +56,42 @@ public class Arbre{
 	 */
 	public Arbre getDroite() {
 		return this.droite;
+	}
+	
+	/**
+	 * retourne le contenu du noeud
+	 * @return elem
+	 */
+	public Object getElem() {
+		return this.elem;
+	}
+	
+	/**
+	 * @return
+	 */
+	public String toString() {
+		return this.elem.toString();
+	}
+	
+	/**
+	 * affiche le contenu de l'arbre a partir du noeud courrant
+	 */
+	public void display() {
+		if (this != null) {
+			this.gauche.display();
+			System.out.println(this.toString());
+			this.droite.display();
+		}
+	}
+	
+	/**
+	 * traduit le morse en lettres
+	 * @param morse chaine de code morse
+	 * @return la chaine de caractères correspondante au code en morse
+	 */
+	public String getLetter(String morse) {
+		String lettre = "";
+		return lettre;
 	}
 	
 	
